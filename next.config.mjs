4aa3ebf7317712bbb0ecbd02b21d7299b294f3e1
Dhruba <zkdhruba@gmail.com>
2026-04-13 02:04:22 +0600

    deployment config fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
   basePath: '/the-mulia-clone',
   assetPrefix: '/the-mulia-clone',
