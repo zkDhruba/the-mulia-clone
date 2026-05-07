4b7d063b27adefabb763d07a81875567c8824412
Dhruba <zkdhruba@gmail.com>
2026-05-08 01:08:28 +0600

    vercel deployement issue fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  output: 'export',
-  // Only apply basePath in production for GitHub Pages
-  basePath: isProd ? '/the-mulia-clone' : '',
-  trailingSlash: true,
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
